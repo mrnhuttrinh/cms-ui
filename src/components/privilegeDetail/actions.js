599604fae9544b276a2d61a1e1cdc70a79972097
Trinh Nguyen Nhut <nntrinh@tma.com.vn>
2018-04-16 23:59:06 +0700

    Issue-1352

--- src/components/privilegeDetail/actions.js
+++ src/components/privilegeDetail/actions.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 import {
   USER_DETAIL_API,
   USER_HISTORY_API,
@@ -114,8 +116,18 @@ export const getRoleList = () =>
   };
 
   export const userUpdateInformation = (data) => {
+    const roles = _.map(data.roles, rl => rl.id);
+    const updateData = {
+      id: data.id,
+      username: data.username,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.email,
+      roles
+    }
     return {
       type: USER_UPDATE_INFORMATION,
+      showRawErrorMessage: true,
       showMessage: {
         success: {
           title: 'Success!',
@@ -135,7 +147,7 @@ export const getRoleList = () =>
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({
-            ...data
+            ...updateData
           }),
         },
       }
